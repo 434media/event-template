a26fe8576a12f1e42a311dddf5b970371d557026
Jesse Hernandez <jesseovr@gmail.com>
2024-10-30 09:37:12 -0500

    partners updated

--- app/components/cta/TechPartners.tsx
+++ app/components/cta/TechPartners.tsx
@@ -87,9 +87,10 @@ export function TechPartners({
       { name: 'Seals', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1730051013/seals_klli1w.svg', alt: 'Seals' },
       { name: 'Google', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1729570438/google_rp9nql.svg', alt: 'Google' },
       { name: 'AWS', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1729570946/amazonwebservices_juiivn.svg', alt: 'AWS' },
-      { name: 'Sputnik ATX', image: 'https://images.squarespace-cdn.com/content/v1/5984957a2994ca9e4bfab3ff/1510851488512-BZPXZI9H3UMLD4WS0W8V/Teal_Sputnik.png?format=1500w', alt: 'Sputnik ATX' },
+      { name: 'UTSA', image: 'https://www.utsa.edu/marcomstudio/images/utsa-wordmark-rowdy-head-signature-left-aligned.svg', alt: 'UTSA' },
       { name: 'Yana', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1730049225/flyers-19-square_3_wdem6d.png', alt: 'Yana' },
       { name: 'balam', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1730048684/flyers-19-square_2_osq1gp.png', alt: 'balam' },
-      { name: 'Dux Capital', image: 'https://media.licdn.com/dms/image/v2/C4E0BAQG2Ky4Ql03whA/company-logo_200_200/company-logo_200_200/0/1674223925710/duxcapital_logo?e=1737590400&v=beta&t=r9HtAfLKQS8Mq0jE6wuucPJ57R3wi0ecZ_Z6764bXfQ', alt: 'Dux Capital' },
+      { name: 'Sputnik ATX', image: 'https://images.squarespace-cdn.com/content/v1/5984957a2994ca9e4bfab3ff/1510851488512-BZPXZI9H3UMLD4WS0W8V/Teal_Sputnik.png?format=1500w', alt: 'Sputnik ATX' },
+      { name: 'Dux Capital', image: 'https://res.cloudinary.com/jessebubble/image/upload/v1730298919/flyers-21-square_4_nnt6c0.png', alt: 'Dux Capital' },
     ],
   };
