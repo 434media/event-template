fc7f0e0068a0bc59f66e4b9c45182729e628454e
Jesse Hernandez <jesseovr@gmail.com>
2025-10-11 22:35:40 -0500

    mt anniversary video mobile

--- app/anniversary/page.tsx
+++ app/anniversary/page.tsx
@@ -5,7 +5,7 @@ export default function AnniversaryPage() {
   return (
     <>
       <Navbar />
-      <main className="h-screen overflow-hidden pt-8 pb-8 md:pt-20 md:pb-16">
+      <main className="h-screen overflow-hidden -mt-16 md:mt-0 pt-8 pb-8 md:pt-20 md:pb-16">
         <AnniversaryHero />
       </main>
     </>
